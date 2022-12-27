18706dd32d586a006381efd9e6b5a17c158d83aa
Sean Gebbett <10674942+SeanGeb@users.noreply.github.com>
2022-12-27 21:32:00 +0000

    Parallelise day16
    ...it was that easy?

--- src/bin/day16/part2.rs
+++ src/bin/day16/part2.rs
@@ -1,4 +1,7 @@
+use std::sync::atomic::{AtomicU32, Ordering};
+
 use petgraph::prelude::*;
+use rayon::prelude::*;
 
 use crate::graph::{build_problem_graph, GraphValve};
 use crate::parse::State;
@@ -24,37 +27,43 @@ fn partition_dfs<'a>(
     let nodes: Vec<_> = graph.nodes().filter(|n| n.name != "AA").collect();
 
     let n_nodes: u32 = graph.nodes().len().try_into().unwrap();
-    let mut best_score = 0;
+
+    let progress = AtomicU32::new(0);
+    let bitvec_max = 1u64.checked_shl(n_nodes - 1).unwrap() - 1;
 
     // NB: can halve the search space to avoid the human and elephant doing the
     // same work.
-    let bitvec_max = 1u64.checked_shl(n_nodes - 1).unwrap() - 1;
-    for partition_bitvec in 0..=bitvec_max {
-        let nodes_human: im_rc::HashSet<GraphValve> = im_rc::HashSet::from_iter(
-            nodes
-                .iter()
-                .enumerate()
-                .filter(|(i, _)| (partition_bitvec & 1 << i) == 0)
-                .map(|(_, n)| *n),
-        );
-        let nodes_elephant: im_rc::HashSet<GraphValve> = im_rc::HashSet::from_iter(
-            nodes
-                .iter()
-                .enumerate()
-                .filter(|(i, _)| (partition_bitvec & 1 << i) != 0)
-                .map(|(_, n)| *n),
-        );
+    let best_score = (0..=bitvec_max)
+        .into_par_iter()
+        .map(|partition_bitvec| -> u32 {
+            let nodes_human: im_rc::HashSet<GraphValve> = im_rc::HashSet::from_iter(
+                nodes
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, _)| (partition_bitvec & 1 << i) == 0)
+                    .map(|(_, n)| *n),
+            );
+            let nodes_elephant: im_rc::HashSet<GraphValve> = im_rc::HashSet::from_iter(
+                nodes
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, _)| (partition_bitvec & 1 << i) != 0)
+                    .map(|(_, n)| *n),
+            );
 
-        let score_human = partition_dfs_helper(graph, from, &nodes_human, 0, time_budget);
-        let score_elephant = partition_dfs_helper(graph, from, &nodes_elephant, 0, time_budget);
-        best_score = best_score.max(score_human + score_elephant);
+            let score_human = partition_dfs_helper(graph, from, &nodes_human, 0, time_budget);
+            let score_elephant = partition_dfs_helper(graph, from, &nodes_elephant, 0, time_budget);
 
-        if partition_bitvec % 256 == 0 {
-            println!("Progress: {partition_bitvec}/{bitvec_max}");
-        }
-    }
+            let progress = progress.fetch_add(1, Ordering::Relaxed);
+            if progress % 256 == 0 {
+                println!("Progress: {progress}/{bitvec_max}");
+            }
 
-    best_score
+            score_human + score_elephant
+        })
+        .max();
+
+    best_score.unwrap()
 }
 
 fn partition_dfs_helper<'a>(
